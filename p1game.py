8814f61c6a7d8bdc882a68bab787ea87a46ab065
B1GC01N <98890511+B1GC01N@users.noreply.github.com>
2022-02-02 17:07:51 +0300

    Add files via upload

---
+++ p1game.py
@@ -0,0 +1,16 @@
+import pygame
+ 
+ 
+w, h = input("введите 1 значение:"), input("введите 2 значение:")
+assert w.isdigit() and h.isdigit(), "Неправильный формат ввода"
+size = w, h = int(w), int(h)
+ 
+ 
+pygame.init()
+surface = pygame.display.set_mode(size)
+pygame.display.set_caption(f"{w}x{h}")
+pygame.draw.line(surface, (255, 255, 255), (0, 0), (w, h), width=5)
+pygame.draw.line(surface, (255, 255, 255), (0, w), (h, 0), width=5)
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.update()
+pygame.quit()
